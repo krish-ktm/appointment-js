c7738e3d32973a3224bc5b447a3785e4a56f7b6e
krish-ktm <ktpatel99@gmail.com>
2025-03-18 22:45:42 +0530

    solved issue of recalculating header

--- src/components/headers/ResponsiveHeader.tsx
+++ src/components/headers/ResponsiveHeader.tsx
@@ -3,21 +3,14 @@ import { MobileHeader } from './MobileHeader';
 import { DesktopHeader } from './DesktopHeader';
 
 export function ResponsiveHeader() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < 640); // 640px is the 'sm' breakpoint in Tailwind
-    };
-
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    return () => window.removeEventListener('resize', checkMobile);
-  }, []);
-
   return (
     <div className="z-40">
-      {isMobile ? <MobileHeader /> : <DesktopHeader />}
+      <div className="lg:hidden">
+        <MobileHeader />
+      </div>
+      <div className="hidden lg:block">
+        <DesktopHeader />
+      </div>
     </div>
   );
 }
